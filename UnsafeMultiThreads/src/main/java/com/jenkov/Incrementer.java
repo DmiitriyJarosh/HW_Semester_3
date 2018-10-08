5d517b28cb11828549de57d52e75e35ab8531564
Dmitriy <dimajarosh@gmail.com>
2018-10-08 17:07:38 +0300

    Add files via upload

---
+++ UnsafeMultiThreads/src/main/java/com/jenkov/Incrementer.java
@@ -0,0 +1,21 @@
+package com.jenkov;
+
+
+public class Incrementer implements Runnable {
+
+    private Counter counter;
+    private Lock lock;
+
+    public Incrementer(Counter counter, Lock lock) {
+        this.counter = counter;
+        this.lock = lock;
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000000; i++) {
+            lock.lock();
+            counter.incCounter();
+            lock.unlock();
+        }
+    }
+}
